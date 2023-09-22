f49cf5fd64a31261a36e6623d076916e0bbe89a2
Bryant Austin <bryantaustin13@yahoo.com>
2023-09-22 14:51:42 -0600

    testing application for Postman collections

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # cqf-tx-testing
-A testing tool for running and reporting on Postman collections.
+This is a node testing tool for running and reporting on Postman collections using the Postman Collection Runner, Newman. This app contains a reporter that returns results in a json file to either a specified output directory or, by default, a results diretory. The output directory will be created, if it doesn't exist.  
+
+## Setting up the environment
+### Environment Variables
+ Either edit workspace.postman_globals.json, replacing '*****' with actual values or create a global Postman environment variables file that contains the following:
+ 
+    {{basicUser}} -- your user name on the server being tested.
+    {{basicPass}} -- your password on the server to be tested.
+    {{SERVER_URL}} -- the url of the server to be tested.
+### Install Newman
+
+Install newman
+
+[Newman](https://learning.postman.com/docs/collections/using-newman-cli/command-line-integration-with-newman/)
+
+    npm i -g newman
+
+Additional [third party newman reporters](https://www.npmjs.com/search?q=newman-reporter) are available to install. This list is maintained by npm. Each reporter from this list has a link to a page that contains instructions on installation and running that reporter.
+
+
+## Running the collection from CLI
+### Running the tests
+
+Running these tests using either Newman or this application runs all the tests and assertions in the collection. One way to use this is to run using the CLI and then look at failing tests, maybe running each individually in Postman to get more details. Importing the collection and environment into Postman would facilitate this. 
+
+#### To run with Newman
+To run using Newman use the following command using the path to the 2 files.
+
+    newman run cqf-measures.postman_collection.json -e workspace.postman_globals.json    
+
+#### To run with this application
+Run npm to build the packages
+
+    npm install
+Install the date-fns package
+    
+    npm install -g date-fns --save
+You may have to run npm install after this to make sure the package is there for node to use it.
+
+There are default parameters in the program that can be set on the commandline.
+    
+    Output directory: './' (current directory).
+    The path and file name of the Postman collection file: '{Absolute path}collections/cqf-measures-terminology-service-tests.postman_collection.json'
+    The path and file name of the environment file where the username, password, and server url are defined: '{Absolute path}/environment/workspace.postman_globals.json';
+
+These will be used unless changed on the command line as (notice no space after the '='). Any that are missing will use the default settings. The output file name will be testResults_yyyyMMddhhmm (year + month + day of year + hour + minute of the beginning of the test run) 
+
+    -op=<your output location>
+    -cs=<the collection location and file name>
+    -es=<the environment settings location and file name>
+
+So the command line would be similar to
+    node cqf-tx-testing.js -cs=/user/collections/newCollection.json -es=/user/environment/postmanEnvironment.json -op=/user/results
+
+Output will be sent to the console and there should be a new json file /user/results/testResults_{time stamp}.json that contains the results.
+
+### Results file format
+The results file consists of summary that contains counts of:
+    
+    Counts of
+        passing assertions
+        failing assertions
+        total assertions
+        tests with no assertions
+        total tests
+    Lists of 
+        passing tests
+        failing tests
+        tests with no assertions
+
+This is followed by a test detail section that consists of
+
+        name of the test
+        each assertion including
+            the actual assertion
+            a message if the assertion failed
+            a result of PASS or FAIL
+
---
+++ cqf-tx-testing.js
@@ -0,0 +1,146 @@
+#!/usr/bin/node
+
+const newman = require('newman');
+const fs = require('fs');
+const colors = require('colors/safe');
+const path = require('path');
+const { format } = require('date-fns');
+
+const currentDate = format(new Date(), 'yyyyMMddhhmm');
+const arguments = process.argv.slice(2);
+
+var directory = '../results'
+var collectionString = '../collections/cqf-measures.postman_collection.json';
+var environmentString = '../collections/workspace.postman_globals.json';
+if(arguments.length > 0){
+    arguments.forEach(arg =>{
+        var prefix = arg.slice(0,4);
+        switch(prefix){
+            case '-cs=':
+                collectionString = arg.slice(4);
+                break;
+            case '-es=':
+                environmentString = arg.slice(4);
+                break;
+            case '-op=':
+                directory = arg.slice(4);
+                break;
+        }
+    });
+}
+newman.run({
+    collection: require(collectionString),
+    environment: require(environmentString)
+}).on('beforeDone', (error, data) => {
+    if (error) {
+        console.log(error);
+    }
+    var TestingResults = {"Summary":{},"TestDetails":[]}
+    var jsonTestTemplate = {"Name": "",
+                             "Assertions": []};
+    var jsonAssertionTemplate = {"Assertion": "", 
+                                "Message": "", 
+                                "Result": ""};
+    var failCount = 0;
+    var passCount = 0;
+    var missingCount = 0;
+    var totalTestCount = 0;
+    var totalAssertionCount = 0;
+    
+    var passingTests = [];
+    var failingTests = [];
+    var missingTests = [];
+
+    data.summary.run.executions.forEach(exec => {
+        totalTestCount++;
+        if (exec.assertions) {
+            console.log('Request name:', exec.item.name);
+            console.log('Assertions:');
+            var addedFail = false;
+            var addedPass = false;
+            var newJsonTest = JSON.parse(JSON.stringify(jsonTestTemplate));
+            newJsonTest.Name = exec.item.name;
+            TestingResults.TestDetails.push(newJsonTest);
+            exec.assertions.forEach(assert => {
+                totalAssertionCount++;
+                var newAssertion = JSON.parse(JSON.stringify(jsonAssertionTemplate));
+                newAssertion.Assertion = assert.assertion;
+                if (assert.error) {
+                    console.log('\t', colors.red('FAILED'), ': ', assert.assertion, '\n\t\t  ', colors.yellow(assert.error.message));
+                    failCount++;
+                    if (!addedFail) {
+                        addedFail = true;
+                    }
+                    newAssertion.Result = 'FAIL';
+                    newAssertion.Message = assert.error.message;
+
+                }
+                else {
+                    console.log('\t', colors.green('PASSED'), ': ', assert.assertion);
+                    passCount++;
+                    if (!addedPass) {
+                        addedPass = true;
+                    }
+                    newAssertion.Result = 'PASS';
+                }
+                newJsonTest.Assertions.push(newAssertion);
+            });
+            if (addedFail) {
+                failingTests.push(exec.item.name);
+            }
+            else {
+                passingTests.push(exec.item.name);
+            }
+        }
+        else{
+            console.log('Request name:', exec.item.name);
+            console.log('\t', colors.red('Missing Assertions'), ': ', exec.item.name);
+            missingCount++;
+            missingTests.push(exec.item.name);
+        }
+    });
+
+    console.log('\n\nSUMMARY\n');
+    console.log('Assertions that passed: ', colors.green(passCount));
+    console.log('Assertions that failed: ', colors.red(failCount));
+    console.log('Total assertions: ', totalAssertionCount);
+    console.log('Tests that are missing assertions: ', colors.red(missingCount));
+    console.log('Total tests: ', totalTestCount);
+    
+    TestingResults.Summary['PassingAssertionsCount'] = passCount;
+    TestingResults.Summary['FailingAssertionsCount'] = failCount;
+    TestingResults.Summary['TotalAssertionCount'] = totalAssertionCount;
+    TestingResults.Summary['MissingAssertionsCount'] = missingCount;
+
+    console.log('Tests that passed');
+    passingTests.forEach(passTest => {
+        console.log('\t', colors.green(passTest));
+    });
+    TestingResults.Summary['TotalTests'] = totalTestCount;
+    TestingResults.Summary['PassingTests'] = passingTests;
+
+    console.log('Tests with failing assertions');
+    failingTests.forEach(failTest => {
+        console.log('\t', colors.red(failTest));
+    });
+    TestingResults.Summary['FailingTests'] = failingTests;
+
+    console.log('Tests with missing assertions');
+    missingTests.forEach(missingTest => {
+        console.log('\t', colors.yellow(missingTest));
+    });
+    TestingResults.Summary['TestsMissingAssertions'] = missingTests;
+
+    var fileName = `testResults_${currentDate}.json`
+
+    if (!fs.existsSync(directory)) {
+        fs.mkdirSync(directory, { recursive: true });
+    }
+     var testResults = {"TestingResults": TestingResults};
+//     testResults['Tests'] = TestingResults;
+
+    var  filePath = path.join(directory, fileName);
+    fs.writeFile(filePath, JSON.stringify(testResults, null, 2), (error)=>{
+        if(error) throw error;
+    });
+}); 
